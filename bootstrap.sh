68f801e7e59eb8d84ddd8e76e1989c35ab16fb80
Beomi <latheledusjp@gmail.com>
2018-11-25 18:43:21 +0900

    bind conda to spark

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
+echo -e "\nexport PYSPARK_PYTHON=/home/hadoop/conda/bin/python" >> /etc/spark/conf/spark-env.sh
+echo "export PYSPARK_DRIVER_PYTHON=/home/hadoop/conda/bin/jupyter" >> /etc/spark/conf/spark-env.sh
+echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'" >> /etc/spark/conf/spark-env.sh
+
 chmod +x install_apt_requirements.sh
 ./install_apt_requirements.sh
 chmod +x install_pip_requirements.sh
