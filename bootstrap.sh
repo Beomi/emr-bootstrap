0b252782f45f156055de3ccfef45293ecb273b55
Beomi <latheledusjp@gmail.com>
2018-11-25 19:16:25 +0900

    Working emr-env.json

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,3 @@
-echo -e "\nexport PYSPARK_PYTHON=/home/hadoop/conda/bin/python" >> $SPARK_HOME/conf/spark-env.sh
-echo "export PYSPARK_DRIVER_PYTHON=/home/hadoop/conda/bin/jupyter" >> $SPARK_HOME/conf/spark-env.sh
-echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'" >> $SPARK_HOME/conf/spark-env.sh
-
 sudo apt-get update
 sudo apt-get install -y git
 
