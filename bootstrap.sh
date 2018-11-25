5a036ebb40272cd660987a5935930c37c6bb67c0
Beomi <latheledusjp@gmail.com>
2018-11-25 18:53:34 +0900

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,14 @@
-echo -e "\nexport PYSPARK_PYTHON=/home/hadoop/conda/bin/python" >> /etc/spark/conf/spark-env.sh
-echo "export PYSPARK_DRIVER_PYTHON=/home/hadoop/conda/bin/jupyter" >> /etc/spark/conf/spark-env.sh
-echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'" >> /etc/spark/conf/spark-env.sh
+echo -e "\nexport PYSPARK_PYTHON=/home/hadoop/conda/bin/python" >> $SPARK_HOME/conf/spark-env.sh
+echo "export PYSPARK_DRIVER_PYTHON=/home/hadoop/conda/bin/jupyter" >> $SPARK_HOME/conf/spark-env.sh
+echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'" >> $SPARK_HOME/conf/spark-env.sh
 
-chmod +x install_apt_requirements.sh
-./install_apt_requirements.sh
-chmod +x install_pip_requirements.sh
-./install_pip_requirements.sh
+sudo apt-get update
+sudo apt-get install -y git
+
+git clone https://github.com/Beomi/emr-bootstrap
+
+chmod +x emr-bootstrap/install_apt_requirements.sh
+./emr-bootstrap/install_apt_requirements.sh
+chmod +x emr-bootstrap/install_pip_requirements.sh
+./emr-bootstrap/install_pip_requirements.sh
 
