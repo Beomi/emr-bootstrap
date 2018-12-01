97482fbbdfd9053a14ef4f12ceebe2315355e71f
Beomi <latheledusjp@gmail.com>
2018-12-01 23:46:04 +0900

    use easy_install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,6 @@
-curl -s https://raw.githubusercontent.com/Beomi/emr-bootstrap/master/install_conda_reqs.sh | sudo docker exec jupyterhub bash
+# curl -s https://raw.githubusercontent.com/Beomi/emr-bootstrap/master/install_conda_reqs.sh | sudo docker exec jupyterhub bash
+
+#!/bin/bash
+
+sudo easy_install-3.4 pip
+sudo /usr/local/bin/pip3 install paramiko nltk scipy scikit-learn pandas
