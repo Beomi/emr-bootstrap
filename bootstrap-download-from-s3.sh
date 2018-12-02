1e78b80d04b8cd37ebe47f356ca450002c14923f
Beomi <latheledusjp@gmail.com>
2018-12-02 12:30:57 +0900

    Download from s3(gzip)

---
+++ bootstrap-download-from-s3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo yum groupinstall -y 'Development Tools'
+sudo yum install -y git python36 python36-pip python36-devel
+
+sudo python36 -m pip install -U pip
+
+sudo python36 -m pip install \
+    paramiko nltk scipy scikit-learn pandas torch==0.4.1 tqdm cmake
+
+sudo cp -r /usr/local/bin/* /usr/bin
+
+export PATH=$PATH:/usr/local/bin
+alias cmake=/usr/local/bin/cmake
+
+sudo wget https://s3.ap-northeast-2.amazonaws.com/simplelink-public-seoul/khaiii.tar.gz
+
+sudo tar -zxvf khaiii.tar.gz
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,1 @@
-curl -s https://raw.githubusercontent.com/Beomi/emr-bootstrap/master/bootstrap-build-khaiii.sh | bash
+curl -s https://raw.githubusercontent.com/Beomi/emr-bootstrap/master/bootstrap-download-from-s3.sh | bash
